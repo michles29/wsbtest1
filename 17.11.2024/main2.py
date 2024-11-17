f20f331d8ac8033261ad309e670f636874362dbf
michles29 <michles29@gmail.com>
2024-11-17 10:10:07 +0100

    zadania python 17.11.2024

---
+++ 17.11.2024/main2.py
@@ -0,0 +1,78 @@
+#zadanie 5
+# x = int(input("Wprowadź dane: "))
+# print(x)
+# result = x/ 2 + 4
+# print(result)
+
+#zadanie 2.1
+# x = input("Wprowadź imię: ")
+# y = input("Wprowadź imię: ")
+
+# print(f'Cześć {y} {x}')
+
+#zadanie 2.2
+# x = input("wprowadź nazwę pliku: ")
+# file = x.split(".")
+# print('Rozszerzenie pliku to: ', file[-1])
+#zadanie 2.3
+# d = int(input("podaj dzień: "))
+# m = int(input("podaj miesiąć: "))
+# y = int(input("podaj rok: "))
+# print(f'{d}/{m}/{y}')
+
+#zadanie 2.4
+# x = int(input("podaj liczbe1: "))
+# y = int(input("podaj liczbe2: "))
+# r = x + y
+# print(f'Suma {x} oraz {y} to {r} ')
+
+#zadanie 2.5
+# x = int(input("podaj liczbe1 całkowita: "))
+# y = int(input("podaj liczbe2 całkowita: "))
+# w = float(input("podaj liczbe3: "))
+# z = float(input("podaj liczbe4: "))
+
+# print(f'typ x to {type(x)}')
+# print(f'typ y to {type(y)}')
+# print(f'typ w to {type(w)}')
+# print(f'typ z to {type(z)}')
+# xw = x + w
+# yz = y - z
+# print(f'typ xw to {type(xw)}')
+# print(f'typ yz to {type(yz)}')
+# print(f'Wynik sumy {xw}, gdzie  i {yz} to {xw + yz}')
+
+#zadanie 2.6
+# liczba = int(input("podaj liczbę: "))
+# stopien = int(input("podaj pierwiastek: "))
+
+# print(f'Pierwiastek stopnia {stopien} z {liczba} to {liczba**(1/stopien)}')
+
+#zadanie 2.7
+# słowo1 = str(input("podaj słowo:"))
+# słowo2 = str(input("podaj słowo:"))
+ 
+# print(f'{słowo1} {słowo2}')
+# print(słowo1, słowo2, sep=", ")
+# print(słowo1, end=" ")
+# print(słowo2)
+
+#zadanie 2.8
+# a = float(input('Podaj pierwszy bok: '))
+# b = float(input('Podaj drugi bok: '))
+# c = float(input('Podaj trzeci bok: '))
+ 
+# p = (a + b + c) / 2
+# pole = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+# print(f'Pole trójkąta wynosi: {pole:.2f}')
+
+#zadanie 2.9
+# km = float(input('Podaj liczbę kilometrów: '))
+# x = 0.621371192
+# mile = km * x
+# print(f'{km:.2f} kilometra to {mile:.2f} mili')
+
+#zadanie 2.10
+# c = float(input('Podaj stopnie Celsjusza: '))
+# x = c*(9/5) + 32
+# print(f'{c} stopni Celsjusza to {x} stopni Fahrenheita')
